f37fd648981a5a2a41b425b2cf6994343287a450
crazyVgod <cw1993cw@163.com>
2013-09-22 22:25:10 +0800

    first

---
+++ 5_7/MAIN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+double power(double x,int y)
+{
+if(y==0) return 1.0;
+return power(x,y-1)*x;
+}
+int main()
+{
+int y;
+double x;
+cin>>x>>y; 
+cout<<power(x,y)<<endl;
+}
